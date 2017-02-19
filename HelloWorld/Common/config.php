3df2a2f20c70528587d86dc3fd0bd8dc7987c1e2
freewl <freewl@qq.com>
2017-02-20 00:37:18 +0800

    update structure;

---
+++ HelloWorld/Common/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: want
+ * Date: 2017/1/22
+ * Time: 23:03
+ */
+$config = array(
+    'type'      =>  'mysql',
+    'host'      =>  '127.0.0.1',
+    'dbname'    =>  'test',
+    'user'      =>  'root',
+    'password'  =>  'freewl',
+    'port'      =>  '3306',
+    'charset'   =>  'utf8'
+);
