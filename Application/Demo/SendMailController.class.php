76b68cf5242affdfe41cd31f9ba84671b2eac957
freewl <freewl@qq.com>
2017-02-20 00:43:30 +0800

    add some files;

---
+++ Application/Demo/SendMailController.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: want
+ * Date: 2017/2/19
+ * Time: 21:19
+ */
+require_once ROOT_PATH.'/HelloWorld/Vendor/PHPMailer-master/PHPMailerAutoload.php';
+
+ class SendMailController{
+
+ }
