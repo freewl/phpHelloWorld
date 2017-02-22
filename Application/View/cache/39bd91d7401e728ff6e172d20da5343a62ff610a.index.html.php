552b0ba56b9363543f58770e5a1bd19ef8aabd11
freewl <freewl@qq.com>
2017-02-22 23:43:36 +0800

    hi;

---
+++ Application/View/cache/39bd91d7401e728ff6e172d20da5343a62ff610a.index.html.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-02-22 15:41:40
+  from "D:\wamp\www\phpHelloWorld\Application\View\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58adb134e53995_50976263',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a9102f2671b6cd94eee7527191bb08687685e265' => 
+    array (
+      0 => 'D:\\wamp\\www\\phpHelloWorld\\Application\\View\\index.html',
+      1 => 1487777933,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 3600,
+),true)) {
+function content_58adb134e53995_50976263 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div style="padding:20px;">
+        Smarty say : "hello world !";
+    </div>
+</body>
+</html><?php }
+}
---
+++ Application/View/compile/a9102f2671b6cd94eee7527191bb08687685e265_0.file.index.html.cache.php
@@ -0,0 +1,39 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-02-22 15:41:40
+  from "D:\wamp\www\phpHelloWorld\Application\View\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58adb134e47e11_40446625',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a9102f2671b6cd94eee7527191bb08687685e265' => 
+    array (
+      0 => 'D:\\wamp\\www\\phpHelloWorld\\Application\\View\\index.html',
+      1 => 1487777933,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_58adb134e47e11_40446625 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '3143958adb134e05785_45102911';
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div style="padding:20px;">
+        <?php echo $_smarty_tpl->tpl_vars['who']->value;?>
+ say : "hello world !";
+    </div>
+</body>
+</html><?php }
+}
